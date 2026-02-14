c6b834f6b59670b3962145f3be4a7bc2fb126184
Collins Emetonjor <42049393+collins-emetonjor@users.noreply.github.com>
2026-02-14 12:00:48 +0000

    ci: add GitHub Actions workflow for backend Python quality checks including linting and formatting.

--- infra/environments/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "lineagelogic-tfstate-rg"
-    storage_account_name = "lineagelogictfstate01"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-    use_oidc             = true
-  }
-}
