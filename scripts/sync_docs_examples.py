ca1dd6fbb3ffe4e8d350d06c84b3370531469b22
Collins Emetonjor <42049393+collins-emetonjor@users.noreply.github.com>
2026-03-01 18:47:31 +0000

    feat: Add a script to synchronize flagship examples to the documentation and update MkDocs navigation to feature new interactive examples.

--- scripts/sync_docs_examples.py
+++ scripts/sync_docs_examples.py
@@ -11,7 +11,7 @@ FLAGSHIP_EXAMPLES = [
     {
         "notebook": "01_quickstart/01_hello_world.ipynb",
         "dest_name": "01_hello_world.ipynb",
-        "supporting_files": [],  # Remote data example - no local files needed
+        "supporting_files": [],
     },
     {
         "notebook": "01_quickstart/02_database_governance.ipynb",
@@ -21,26 +21,16 @@ FLAGSHIP_EXAMPLES = [
         ],
     },
     {
-        "notebook": "04_compliance_governance/hipaa_pii_masking/tutorial_hipaa_gdpr_compliance.ipynb",
+        # Renamed: 04_compliance_governance/hipaa_pii_masking
+        #       -> 03_compliance_governance/hipaa_gdpr_pii_masking
+        "notebook": "03_compliance_governance/hipaa_gdpr_pii_masking/tutorial_hipaa_gdpr_compliance.ipynb",
         "dest_name": "hipaa_gdpr_compliance.ipynb",
-        "supporting_files": [],  # Uses inline data generation
-    },
-    {
-        "notebook": "03_advanced_workflows/ai_contract_enrichment/ai_enrich_demo.ipynb",
-        "dest_name": "ai_enrich_demo.ipynb",
         "supporting_files": [],
     },
     {
-        "notebook": "03_advanced_workflows/ai_contract_enrichment/ai_edge_cases_demo.ipynb",
-        "dest_name": "ai_edge_cases_demo.ipynb",
+        "notebook": "02_core_patterns/bronze_quality_gate/playbook.ipynb",
+        "dest_name": "bronze_quality_gate.ipynb",
         "supporting_files": [],
-    },
-    {
-        "notebook": "03_advanced_workflows/synthetic_data_generation/synthetic_data_generation.ipynb",
-        "dest_name": "synthetic_data_generation.ipynb",
-        "supporting_files": [
-            ("03_advanced_workflows/synthetic_data_generation/contract_orders.yaml", "contract_orders.yaml"),
-        ],
     },
     {
         "notebook": "02_core_patterns/medallion_architecture/quickstart_tutorial.ipynb",
@@ -48,13 +38,23 @@ FLAGSHIP_EXAMPLES = [
         "supporting_files": [],
     },
     {
-        "notebook": "03_advanced_workflows/late_arriving_reprocess/demo_self_healing.ipynb",
-        "dest_name": "late_arriving_reprocess.ipynb",
+        "notebook": "02_core_patterns/dedup_survivorship/playbook.ipynb",
+        "dest_name": "dedup_survivorship.ipynb",
         "supporting_files": [],
     },
     {
-        "notebook": "03_advanced_workflows/payments_lifecycle/payments_lifecycle.ipynb",
-        "dest_name": "payments_lifecycle.ipynb",
+        "notebook": "02_core_patterns/scd2_dimension/playbook.ipynb",
+        "dest_name": "scd2_dimension.ipynb",
+        "supporting_files": [],
+    },
+    {
+        "notebook": "02_core_patterns/soft_delete/playbook.ipynb",
+        "dest_name": "soft_delete.ipynb",
+        "supporting_files": [],
+    },
+    {
+        "notebook": "02_core_patterns/reference_joins/playbook.ipynb",
+        "dest_name": "reference_joins.ipynb",
         "supporting_files": [],
     },
 ]
